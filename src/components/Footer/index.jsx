c44eb7908e460c3479b70db6e7c3d4c930e0a867
KOUAMLAN <evlekouamlanwilliams@yahoo.fr>
2025-07-20 19:52:15 +0200

    ✅ fix: déplacement de Footer.jsx dans src/components/Footer/

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer data-testid="footer">
+      <p>© 2024 Votre société</p>
+    </footer>
+  );
+};
+
+export default Footer;
