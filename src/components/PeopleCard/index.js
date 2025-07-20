2460ec61cd2340cf42d8ae6543afa7175d342949
KOUAMLAN <evlekouamlanwilliams@yahoo.fr>
2025-07-20 19:56:11 +0200

    ✅ fix: correction du test PeopleCard pour utiliser prop 'role' au lieu de 'position'

--- src/components/PeopleCard/index.js
+++ src/components/PeopleCard/index.js
@@ -1,28 +1,29 @@
+import React from "react";
 import PropTypes from "prop-types";
-
 import "./style.scss";
 
-const PeopleCard = ({ imageSrc, imageAlt, position, name }) => (
+const PeopleCard = ({ name, role, imageSrc }) => {
+  return (
     <div className="PeopleCard">
       <div className="PeopleCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+        <img src={imageSrc || ""} alt="" data-testid="card-image-testid" />
       </div>
       <div className="PeopleCard__descriptionContainer">
         <div className="PeopleCard__name">{name}</div>
-        <div className="PeopleCard__position">{position}</div>
+        <div className="PeopleCard__position">{role}</div> {/* 👈 VERY IMPORTANT */}
       </div>
     </div>
   );
+};
 
 PeopleCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
-  imageAlt: PropTypes.string,
   name: PropTypes.string.isRequired,
-  position: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
 };
 
 PeopleCard.defaultProps = {
-  imageAlt: "",
-}
+  imageSrc: "",
+};
 
 export default PeopleCard;
