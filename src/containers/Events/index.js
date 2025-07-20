f41505da8f3d0fc60e757ee1642515353f707f78
KOUAMLAN <evlekouamlanwilliams@yahoo.fr>
2025-07-20 11:46:20 +0200

    Fix: correction de la pagination sur le listing d'événements

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -4,7 +4,6 @@ import Select from "../../components/Select";
 import { useData } from "../../contexts/DataContext";
 import Modal from "../Modal";
 import ModalEvent from "../ModalEvent";
-
 import "./style.css";
 
 const PER_PAGE = 9;
@@ -13,65 +12,67 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      // CORRECTION : Ajout de ".filter" pour filtrer les évenements par catégories
-      : data?.events.filter(event => event.type === type)) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+
+  const eventsFiltered = !type
+    ? data?.events || []
+    : data?.events.filter(event => event.type === type) || [];
+
+  const totalPages = Math.ceil(eventsFiltered.length / PER_PAGE);
+
+  const startIndex = (currentPage - 1) * PER_PAGE;
+  const endIndex = startIndex + PER_PAGE;
+  const eventsOnPage = eventsFiltered.slice(startIndex, endIndex);
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+
   const typeList = new Set(data?.events.map((event) => event.type));
+
   return (
     <>
       {error && <div>An error occured</div>}
-      {data === null ? (
-        "loading"
-      ) : (
-        <>
-          <h3 className="SelectTitle">Catégories</h3>
-          <Select
-            selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
-          />
-          <div id="events" className="ListContainer" > 
-            {filteredEvents.map((event) => (
-              <Modal key={event.id} Content={<ModalEvent event={event} />}>
-                {({ setIsOpened }) => (
-                  <EventCard
-                    onClick={() => setIsOpened(true)}
-                    imageSrc={event.cover}
-                    title={event.title}
-                    date={new Date(event.date)}
-                    label={event.type}
-                  />
-                )}
-              </Modal>
-            ))}
-          </div>
-          <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
-                {n + 1}
-              </a>
-            ))}
-          </div>
-        </>
-      )}
+      {data === null
+        ? "loading"
+        : (
+          <>
+            <h3 className="SelectTitle">Catégories</h3>
+            <Select
+              selection={Array.from(typeList)}
+              onChange={value => value ? changeType(value) : changeType(null)}
+            />
+            <div id="events" className="ListContainer">
+              {eventsOnPage.map((event) => (
+                <Modal key={event.id} Content={<ModalEvent event={event} />}>
+                  {({ setIsOpened }) => (
+                    <EventCard
+                      onClick={() => setIsOpened(true)}
+                      imageSrc={event.cover}
+                      title={event.title}
+                      date={new Date(event.date)}
+                      label={event.type}
+                    />
+                  )}
+                </Modal>
+              ))}
+            </div>
+            <div className="Pagination">
+              {[...Array(totalPages)].map((_, n) => (
+                <a
+                  key={n}
+                  href="#events"
+                  className={n + 1 === currentPage ? "active" : ""}
+                  onClick={() => setCurrentPage(n + 1)}
+                >
+                  {n + 1}
+                </a>
+              ))}
+            </div>
+          </>
+        )
+      }
     </>
   );
 };
 
-export default EventList;
